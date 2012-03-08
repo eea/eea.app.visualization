a08c6286d17528bdb9250758add2e19bacfdf398
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-03-08 13:01:39 +0000

    Refs #4996   - Moved facets API from eea.daviz to eea.app.visualization

---
+++ eea/app/visualization/facets/events.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+""" Facets events module
+"""
+__author__ = """European Environment Agency (EEA)"""
+__docformat__ = 'plaintext'
+__credits__ = """contributions: Alec Ghica, Alin Voinea"""
+
+import logging
+from zope.component import queryAdapter
+from eea.app.visualization.interfaces import IVisualizationConfig
+logger = logging.getLogger('eea.app.visualization.facets.events')
+
+def create_default_facets(obj, evt):
+    """ Create default facets
+    """
+    mutator = queryAdapter(obj, IVisualizationConfig)
+    if not mutator:
+        logger.warn("Couldn't find any IVisualizationConfig adapter for %s",
+                    obj.absolute_url(1))
+        return
+
+    if evt.cleanup:
+        # Remove all facets
+        mutator.delete_facets()
+
+    # Add new facets or edit existing
+    for facet, typo in evt.columns:
+        if not mutator.facet(facet):
+            show = ('label' not in facet) or ('id' not in facet)
+            mutator.add_facet(
+                name=facet, label=facet, show=show, item_type=typo)
+        else:
+            mutator.edit_facet(facet, item_type=typo)
