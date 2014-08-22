9e85f8c6a1b013e17ffdff83e72fb6dfcfb9e2f1
Alin Voinea <alin@eaudeweb.ro>
2014-08-22 16:50:36 +0300

    [JENKINS] - Fix ImportError

--- eea/app/visualization/cache/cache.py
+++ eea/app/visualization/cache/cache.py
@@ -5,3 +5,8 @@ def cacheJsonKey(method, self, *args, **kwargs):
     """
     name = getattr(self, '__name__', '')
     return ':'.join((self.context.absolute_url(1), name))
+
+
+class InvalidateCacheEvent(object):
+    """ his event will be raised if there is no cache support
+    """
