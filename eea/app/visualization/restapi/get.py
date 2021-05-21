21b8575106b0e7b92c082ea8597bbabb1c6aea79
Petchesi-Iulian <iamnotacat12@gmail.com>
2021-05-21 13:51:14 +0200

    Refs #123935 Added POST for dataprovenance + pyflakes.

--- eea/app/visualization/restapi/get.py
+++ eea/app/visualization/restapi/get.py
@@ -1,13 +1,12 @@
-""" RestAPI enpoint @geolocation GET
+""" RestAPI enpoint @dataprovenance GET
 """
-from plone.restapi.services import Service
-from zope.component import getUtility, queryUtility
-from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
-from plone.restapi.interfaces import IExpandableElement
-from zope.component import adapter
+from zope.interface import implementer
 from zope.interface import Interface
+from zope.component import adapter
+from plone.restapi.services import Service
 from plone.restapi.serializer.converters import json_compatible
+from plone.restapi.interfaces import IExpandableElement
 from eea.app.visualization.interfaces import IDataProvenance
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 
@@ -15,7 +14,7 @@ from Products.CMFPlone.interfaces import IPloneSiteRoot
 @implementer(IExpandableElement)
 @adapter(Interface, Interface)
 class DataProvenance(object):
-    """ Get workflow progress
+    """ Get dataprovenance
     """
     def __init__(self, context, request):
         self.context = context
--- eea/app/visualization/restapi/post.py
+++ eea/app/visualization/restapi/post.py
@@ -1,44 +1,68 @@
-""" RestAPI enpoint @geolocation GET
+""" RestAPI enpoint @dataprovenance POST
 """
-# pylint: disable = W0702
-# pylint: disable = W0612
-from plone import api
-from plone.restapi.services import Service
-from zope.component import getUtility, queryUtility
-from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
-from plone.restapi.interfaces import IExpandableElement
-from zope.component import adapter
+from zope.interface import implementer, alsoProvides
 from zope.interface import Interface
-from plone.restapi.serializer.converters import json_compatible
+from zope.component import adapter
+from plone.restapi.services import Service
+from plone.restapi.interfaces import IExpandableElement
+from plone.restapi.deserializer import json_body
 from eea.app.visualization.interfaces import IDataProvenance
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+
+
+import plone.protect.interfaces
 
 
 @implementer(IExpandableElement)
 @adapter(Interface, Interface)
 class DataProvenance(object):
-    """ Get workflow progress
+    """ Set DataProvenance
     """
     def __init__(self, context, request):
         self.context = context
         self.request = request
 
-    def __call__(self):
-        result = {}
-        import pdb; pdb.set_trace()
+    def __call__(self, data={}):
         if IPloneSiteRoot.providedBy(self.context):
-            return result
+            self.request.response.setStatus(400)
+            return dict(error=dict(type="BadRequest", message="Tried to set dataprovenance on site root."))
 
         source = IDataProvenance(self.context)
-        return result
+        source.title = data["@title"]
+        source.owner = data["@owner"]
+        source.title = data["@link"]
+
+        if "@copyrights" in data and hasattr(source, "copyrights"):
+            copyrights = data['@copyrights']
+
+            if len(copyrights) > 2 and isinstance(copyrights, list):
+                self.request.response.setStatus(400)
+                return dict(error=dict(type="BadRequest", message="Copyrights must be list with < 2 items or string."))
+
+            if isinstance(copyrights, (str, unicode)):
+                source.copyrights = copyrights
+            source.copyrights = tuple(copyrights)
+        else:
+            self.request.response.setStatus(400)
+            return dict(error=dict(type="BadRequest", message="Can't set copyrights, not a blob object."))
+
+        self.request.response.setStatus(200)
+        return dict(message="Successfully set dataprovenance")
+
 
 
 @implementer(IPublishTraverse)
-class Get(Service):
-    """GET"""
+class Post(Service):
+    """POST"""
 
     def reply(self):
         """Reply"""
-        import pdb; pdb.set_trace()
-        data = DataProvenance(self.context, self.request)
-        return data
+        data = json_body(self.request)
+
+        # Disable CSRF protection
+        if "IDisableCSRFProtection" in dir(plone.protect.interfaces):
+            alsoProvides(self.request, plone.protect.interfaces.IDisableCSRFProtection)
+
+        post = DataProvenance(self.context, self.request)
+        return post(data)
