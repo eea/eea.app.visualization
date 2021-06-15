a8affc49b07fab9c029333192c887e9501c08706
Petchesi-Iulian <iamnotacat12@gmail.com>
2021-06-15 11:54:25 +0200

    Refs #123935 Added MultiDataProvenance to api GET .

--- eea/app/visualization/restapi/get.py
+++ eea/app/visualization/restapi/get.py
@@ -7,7 +7,7 @@ from zope.component import adapter
 from plone.restapi.services import Service
 from plone.restapi.serializer.converters import json_compatible
 from plone.restapi.interfaces import IExpandableElement
-from eea.app.visualization.interfaces import IDataProvenance
+from eea.app.visualization.interfaces import IDataProvenance, IMultiDataProvenance
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 
 
@@ -21,19 +21,30 @@ class DataProvenance(object):
         self.request = request
 
     def __call__(self):
-        result = {}
+        result = []
 
         if IPloneSiteRoot.providedBy(self.context):
             return result
 
         source = IDataProvenance(self.context)
-        result.update({"@title": json_compatible(source.title)})
-        result.update({"@owner": json_compatible(source.owner)})
-        result.update({"@link": json_compatible(source.link)})
+
+        result.append({
+            "title": json_compatible(source.title),
+            "owner": json_compatible(source.owner),
+            "link": json_compatible(source.link)
+        })
 
         if hasattr(source, "copyrights"):
-            result.update({"@copyrights": json_compatible(source.copyrights)})
+            result[0].update({"copyrights": json_compatible(source.copyrights)})
 
+        # also get IMultiDataProvenance
+        multi = IMultiDataProvenance(self.context)
+        provenances = json_compatible(multi.provenances)
+
+        if hasattr(multi, "copyrights"):
+            provenances[0].update({"copyrights": json_compatible(multi.copyrights)})
+
+        result += provenances
         return result
 
 
